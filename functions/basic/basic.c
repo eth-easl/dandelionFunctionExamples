363d2810314597f2369ea57dff6fd660d8fcefbc
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2022-06-22 17:52:27 +0200

    first trail working with function capability and returning to pair

--- functions/basic/basic.c
+++ functions/basic/basic.c
@@ -14,3 +14,14 @@ int returnNumber(int number){
 int addOne(int number){
   return number+1;
 }
+
+void preWrapped(void){
+  // return using the load pair at the bottom of the ddc
+  // puts the contex capability in c0
+  __asm__ volatile(
+    "mrs c0, ddc \n"
+    "ldr c0, [c0] \n"
+    "ldpbr c0, [c0] \n"
+    : : : "c0"
+  );
+}
--- functions/basic/basic.h
+++ functions/basic/basic.h
@@ -5,3 +5,7 @@ int returnSeven();
 int returnNumber(int number);
 
 int addOne(int number);
+
+// function for prototyping that allready contains all the code that should be
+// added by dandelion for wrapping the function
+void preWrapped(void);
