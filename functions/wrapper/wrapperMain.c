9d50e0f7648b364c5cf4586811090f42aecdc1fa
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2022-08-05 09:37:17 +0200

    Change wrapper to perform return via loadpair

--- functions/wrapper/wrapperMain.c
+++ functions/wrapper/wrapperMain.c
@@ -19,8 +19,15 @@ int main(void) {
   test_initialized = 7;
   test_uninitialized = 8;
   testFunc();
-  printf("%d\n", test_initialized);
-  printf("%d\n", test_uninitialized);
-  printf("%d\n", test_inFunc);
-  return 0;
+  // printf("%d\n", test_initialized);
+  // printf("%d\n", test_uninitialized);
+  // printf("%d\n", test_inFunc);
+  // return 7;
+
+  __asm__ volatile(
+    "mrs c0, ddc \n"
+    "ldr c0, [c0] \n"
+    "ldpbr c29, [c0] \n"
+  );
+  __builtin_unreachable();
 }
