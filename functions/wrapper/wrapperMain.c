f780b2b229fede2dc2d2c2803569d1236b7576c1
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2022-08-12 13:50:53 +0200

    Add function interface and first working prototype in server

--- functions/wrapper/wrapperMain.c
+++ functions/wrapper/wrapperMain.c
@@ -5,11 +5,13 @@
 // Project External Libraries
 
 // Project Internal Libraries
-// #include "dandelionIO.h"
+#include "dandelionIO.h"
 
 int test_initialized = 5;
 int test_uninitialized;
 int test_inFunc = 6;
+void* debugSymbol = NULL;
+long int debugSymbol2 = 2;
 
 void testFunc(void){
   test_inFunc = 9;
@@ -19,6 +21,17 @@ int main(void) {
   test_initialized = 7;
   test_uninitialized = 8;
   testFunc();
+
+  size_t inputSize = getInputSize(0);
+  int* inputPointer = getInputPointer(0);
+
+  addOutput();
+  int outPutNumber = 11* (*inputPointer);
+  debugSymbol = &outPutNumber;
+  debugSymbol2 = outPutNumber;
+  setOutputSize(0, 4);
+  setOutputPointer(0, &outPutNumber);
+
   // printf("%d\n", test_initialized);
   // printf("%d\n", test_uninitialized);
   // printf("%d\n", test_inFunc);
