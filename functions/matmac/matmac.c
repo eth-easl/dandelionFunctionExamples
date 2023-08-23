8eac0ac7aac004233bc7a74dcc771d0dfeb7dca1
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2023-08-23 09:29:45 +0200

    Update to take single input item per matrix

--- functions/matmac/matmac.c
+++ functions/matmac/matmac.c
@@ -13,42 +13,53 @@
 int mat_mac() {
   // check we get expected inputs
   size_t input_set_count = dandelion_input_set_count();
-  if (input_set_count > 4) return -1;
-  size_t size_set_size = dandelion_input_buffer_count(0);
-  if (size_set_size != 2) return -2;
-  size_t a_set_size = dandelion_input_buffer_count(1);
-  size_t b_set_size = dandelion_input_buffer_count(2);
-  size_t c_set_size = dandelion_input_buffer_count(3);
-
-  struct io_buffer* row_buffer = dandelion_get_input(0, 0);
-  if (row_buffer->data_len != 8) return -4;
-  size_t mat_rows = *(int64_t*)row_buffer->data;
-  struct io_buffer* col_buffer = dandelion_get_input(0, 1);
-  if (col_buffer->data_len != 8) return -5;
-  size_t mat_cols = *(int64_t*)col_buffer->data;
+  if (input_set_count != 3) return -1;
+  size_t a_set_size = dandelion_input_buffer_count(0);
+  size_t b_set_size = dandelion_input_buffer_count(1);
+  size_t c_set_size = dandelion_input_buffer_count(2);
 
   int64_t* out_mat;
+  int64_t mat_rows, mat_cols;
+
+  // sanity check all available matrices for their size information
+  for (int i = 0; i < 3; i++) {
+    size_t set_size = dandelion_input_buffer_count(i);
+    if (set_size == 1) {
+      struct io_buffer* mat_buffer = dandelion_get_input(i, 0);
+      size_t mat_size = mat_buffer->data_len;
+      int64_t mat_rows = ((int64_t*)mat_buffer->data)[0];
+      if (mat_size < 8 || mat_size % 8 != 0) return -2;
+      mat_size = mat_size / 8 - 1;
+      if (mat_rows > mat_size || mat_size % mat_rows != 0) return -3;
+    }
+  }
+
   if (c_set_size == 1) {
-    struct io_buffer* c_mat_buffer = dandelion_get_input(3, 0);
-    if (c_mat_buffer->data_len != mat_rows * mat_cols * 8) return -6;
-    out_mat = (int64_t*)c_mat_buffer->data;
+    struct io_buffer* c_mat_buffer = dandelion_get_input(2, 0);
+    int64_t mat_size = (c_mat_buffer->data_len - 8) / 8;
+    int64_t* c_mat = c_mat_buffer->data;
+    mat_rows = c_mat[0];
+    mat_cols = mat_size / mat_rows;
+    out_mat = c_mat + 1;
   } else {
-    out_mat = dandelion_alloc(mat_rows * mat_cols * 8, 8);
+    if (a_set_size != 1 || b_set_size != 1) return -4;
+    struct io_buffer* a_mat_buffer = dandelion_get_input(0, 0);
+    struct io_buffer* b_mat_buffer = dandelion_get_input(1, 0);
+    mat_rows = ((int64_t*)a_mat_buffer->data)[0];
+    mat_cols =
+        ((b_mat_buffer->data_len - 8) / 8) / ((int64_t*)b_mat_buffer->data)[0];
+    out_mat = dandelion_alloc((mat_rows * mat_cols + 1) * 8, 8);
+    out_mat++;
   }
 
   if (a_set_size == 1 && b_set_size == 1) {
-    struct io_buffer* a_mat_buffer = dandelion_get_input(1, 0);
-    struct io_buffer* b_mat_buffer = dandelion_get_input(2, 0);
-    if (a_mat_buffer->data_len % 8 != 0 ||
-        (a_mat_buffer->data_len / 8) % mat_rows != 0)
-      return -7;
-    size_t common_length = (a_mat_buffer->data_len / 8) / mat_rows;
-    if (b_mat_buffer->data_len % 8 != 0 ||
-        (b_mat_buffer->data_len / 8) % mat_cols != 0 ||
-        (b_mat_buffer->data_len / 8) % common_length != 0)
-      return -8;
+    struct io_buffer* a_mat_buffer = dandelion_get_input(0, 0);
+    struct io_buffer* b_mat_buffer = dandelion_get_input(1, 0);
+    size_t common_length = ((a_mat_buffer->data_len - 1) / 8) / mat_rows;
     int64_t* a_mat = a_mat_buffer->data;
+    a_mat++;
     int64_t* b_mat = b_mat_buffer->data;
+    b_mat++;
     for (int64_t i = 0; i < mat_rows; i++) {
       for (int64_t j = 0; j < mat_cols; j++) {
         for (int64_t k = 0; k < common_length; k++) {
@@ -60,37 +71,24 @@ int mat_mac() {
   } else {
     int64_t* add_mat;
     if (a_set_size == 1 && b_set_size == 0) {
-      struct io_buffer* a_mat_buffer = dandelion_get_input(1, 0);
-      if (a_mat_buffer->data_len != mat_rows * mat_cols * 8) return -9;
+      struct io_buffer* a_mat_buffer = dandelion_get_input(0, 0);
       add_mat = a_mat_buffer->data;
     } else if (a_set_size == 0 && b_set_size == 1) {
-      struct io_buffer* b_mat_buffer = dandelion_get_input(2, 0);
-      if (b_mat_buffer->data_len != mat_rows * mat_cols * 8) return -9;
+      struct io_buffer* b_mat_buffer = dandelion_get_input(1, 0);
       add_mat = b_mat_buffer->data;
     } else
-      return -10;
+      return -5;
+    add_mat++;
     for (int64_t i = 0; i < mat_rows * mat_cols; i++) {
       out_mat[i] += add_mat[i];
     }
   }
 
-  int64_t* out_dims = dandelion_alloc(sizeof(int64_t) * 2, 8);
-  out_dims[0] = mat_rows;
-  out_dims[1] = mat_cols;
-  struct io_buffer out_row_buffer = {0};
-  out_row_buffer.data = &out_dims[0];
-  out_row_buffer.data_len = sizeof(int64_t);
-  dandelion_add_output(0, out_row_buffer);
-
-  struct io_buffer out_col_buffer = {0};
-  out_col_buffer.data = &out_dims[1];
-  out_col_buffer.data_len = sizeof(int64_t);
-  dandelion_add_output(0, out_col_buffer);
-
   struct io_buffer out_mat_buffer = {0};
-  out_mat_buffer.data = out_mat;
-  out_mat_buffer.data_len = mat_rows * mat_cols * 8;
-  dandelion_add_output(1, out_mat_buffer);
+  out_mat_buffer.data = --out_mat;
+  out_mat[0] = mat_rows;
+  out_mat_buffer.data_len = (mat_rows * mat_cols + 1) * 8;
+  dandelion_add_output(0, out_mat_buffer);
 
   return 0;
 }
--- functions/matmul/matmul.c
+++ functions/matmul/matmul.c
@@ -10,33 +10,36 @@
 int mat_mul() {
   // check we get expected inputs
   size_t input_set_count = dandelion_input_set_count();
-  if (input_set_count < 2) return -1;
-  size_t size_set_size = dandelion_input_buffer_count(0);
-  if (size_set_size != 1) return -2;
-  size_t mat_set_size = dandelion_input_buffer_count(1);
-  if (mat_set_size != 1) return -3;
+  if (input_set_count != 1) return -1;
+  size_t mat_set_size = dandelion_input_buffer_count(0);
+  if (mat_set_size != 1) return -2;
 
-  struct io_buffer* size_buffer = dandelion_get_input(0, 0);
-  struct io_buffer* in_mat_buffer = dandelion_get_input(1, 0);
-  if (size_buffer->data_len != 8) return -4;
-  size_t mat_size = *(size_t*)size_buffer->data;
-  if (in_mat_buffer->data_len != mat_size * mat_size * 8) return -5;
+  struct io_buffer* mat_buffer = dandelion_get_input(0, 0);
+  if (mat_buffer->data_len < 8) return -3;
+  int64_t mat_rows = *(int64_t*)mat_buffer->data;
+  size_t mat_size = mat_buffer->data_len - 8;
+  if (mat_size < 8 || mat_size % 8 != 0 || (mat_size / 8) % mat_rows != 0)
+    return -4;
+  int64_t mat_cols = (mat_size / 8) / mat_rows;
 
-  int64_t* in_mat = in_mat_buffer->data;
-  int64_t* out_mat = dandelion_alloc(in_mat_buffer->data_len, 8);
+  int64_t* in_mat = (int64_t*)mat_buffer->data + 1;
+  int64_t* out_mat = dandelion_alloc(mat_rows * mat_rows * 8 + 8, 8);
+  out_mat[0] = mat_rows;
+  out_mat++;
 
-  for (int i = 0; i < mat_size; i++) {
-    for (int j = 0; j < mat_size; j++) {
-      for (int k = 0; k < mat_size; k++) {
-        out_mat[i * mat_size + j] +=
-            in_mat[i * mat_size + k] * in_mat[j * mat_size + k];
+  for (int i = 0; i < mat_rows; i++) {
+    for (int j = 0; j < mat_rows; j++) {
+      for (int k = 0; k < mat_cols; k++) {
+        out_mat[i * mat_rows + j] +=
+            in_mat[i * mat_cols + k] * in_mat[j * mat_cols + k];
       }
     }
   }
 
   struct io_buffer out_mat_buffer = {0};
-  out_mat_buffer.data = out_mat;
-  out_mat_buffer.data_len = in_mat_buffer->data_len;
+  out_mat_buffer.data = out_mat - 1;
+  out_mat_buffer.key = mat_buffer->key;
+  out_mat_buffer.data_len = mat_rows * mat_rows * 8 + 8;
 
   dandelion_add_output(0, out_mat_buffer);
   return 0;
