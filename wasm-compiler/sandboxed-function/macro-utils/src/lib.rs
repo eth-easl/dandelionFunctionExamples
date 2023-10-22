656d54f018175f15bf29f489d919d26844dd2e89
leon-thomm <l.thomm@mailbox.org>
2023-10-22 17:20:23 +0200

    clean macro-utils

--- wasm-compiler/sandboxed-function/macro-utils/src/lib.rs
+++ wasm-compiler/sandboxed-function/macro-utils/src/lib.rs
@@ -1,12 +1,7 @@
-// #![no_std]
-
 extern crate sandbox_generated;
 use std::str::FromStr;
 
 use sandbox_generated as Generated;
-
-// extern crate proc_macro;
-// use proc_macro2::TokenStream;
 
 //
 // Helper macros to extract data necessary for Dandelion from the generated
@@ -14,10 +9,9 @@ use sandbox_generated as Generated;
 //
 
 #[proc_macro]
-pub fn get_system_data(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn get_system_data_wasm_offset(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let module = Generated::WasmModule::new();
     let system_data = module.get___dandelion_system_data().unwrap();
     let system_data = format!("0x{:x}", system_data);
-    // let system_data = syn::LitStr::new(&system_data, proc_macro2::Span::call_site());
     proc_macro::TokenStream::from_str(&system_data).unwrap()
 }
