23868d0a8aad54c5c5af5a83acc59f27cd3902d3
Tom Kuchler <tom.kuchler@inf.ethz.ch>
2026-01-28 13:06:23 +0100

    Update basic function to make it easier to observe it ran correctly

--- c_functions/basic/basic.c
+++ c_functions/basic/basic.c
@@ -1,7 +1,12 @@
 #include "dandelion/runtime.h"
 
-// most basic function that does nothing except for returning
+char output = 0;
+
+// most basic function that does nothing except for returning a simple fixed
+// item
 void _start(void) {
   dandelion_init();
+  IoBuffer new_out = {.data = &output, .data_len = 1, .ident_len = 0, .key = 0};
+  dandelion_add_output(0, new_out);
   dandelion_exit(0);
 }
